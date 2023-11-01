8fd9a8b92578f451ef31fcae9b15ff865b90f710
Xer1970 <132815768+Xer1970@users.noreply.github.com>
2023-11-01 22:50:46 +0800

    Update anime.js

--- js/anime.js
+++ js/anime.js
@@ -1,10 +1,10 @@
 // Api urls
 
-const infoapi = "https://consumet-api-qdef.onrender.com/anime/gogoanime/info/";
+const infoapi = "https://webdis-k35y.onrender.com/anime/gogoanime/info/";
 const epapi = "https://api.techzbots1.workers.dev/gogo/episodes/";
-const searchapi = "https://consumet-api-qdef.onrender.com/meta/anilist/";
-const anilistinfoapi = "https://consumet-api-qdef.onrender.commeta/anilist/info/";
-const gogosearchapi = "https://consumet-api-qdef.onrender.com/anime/gogoanime/";
+const searchapi = "https://webdis-k35y.onrender.com/meta/anilist/";
+const anilistinfoapi = "https://webdis-k35y.onrender.com/anilist/info/";
+const gogosearchapi = "https://webdis-k35y.onrender.com/anime/gogoanime/";
 
 // Usefull functions
 
